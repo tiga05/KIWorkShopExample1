a27b12c2ba492163e0ed735fed3cc700e3dba4b6
Tim Gaudich <tim.gaudich@heidelberg.com>
2021-06-30 14:08:28 +0200

    cleanup of game_controller

--- game_controller.py
+++ game_controller.py
@@ -6,24 +6,29 @@ import pyautogui
 import tensorflow as tf
 import sys
 import numpy as np
-
 from tensorflow.python.saved_model.load import load
-
 sys.path.append("research")
 sys.path.append("research\\object_detection\\utils")
 from object_detection.utils import ops as utils_ops
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
+####################################################################
 
-PATH_TO_LABELS = 'labelmap.pbtxt'
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
-model_path = 'saved_model_colab2'
+
+PATH_TO_LABELS = 'labelmap.pbtxt'
+PATH_TO_MODEL = 'savedModel'
+
+###################################################################
+#load model
 def load_model(model_path):
     model = tf.saved_model.load(model_path)
     return model
 
+###################################################################
+#recognition for one image
 def run_inference_for_single_image(model, image):
     image = np.asarray(image)
     # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
@@ -32,7 +37,7 @@ def run_inference_for_single_image(model, image):
     input_tensor = input_tensor[tf.newaxis,...]
     
     # Run inference
-    output_dict = model(input_tensor)
+    output_dict= model(input_tensor)
 
     # All outputs are batches tensors.
     # Convert to numpy arrays, and take index [0] to remove the batch dimension.
@@ -58,14 +63,48 @@ def run_inference_for_single_image(model, image):
 
 
 # List of the strings that is used to add correct label for each box.
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
 
-category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
-loadedModell = load_model(model_path)
-category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+#load model
+loadedModell = load_model(PATH_TO_MODEL)
+
+#####################################
+# processing loop; runs endless until "q" is pushed
+# classes:
+# 1 == tennisball
+# 2 == finger
+# 3 == laugh
 
 while True:
     ret, image = cap.read()
     output_dict = run_inference_for_single_image(loadedModell, image)
+
+####################### good place for your modifications #############################
+    labelList = []
+    index = 0
+
+    for detection in output_dict['detection_scores']:
+        if(detection > 0.55):
+            labelNumber = output_dict['detection_classes'][index]
+            labelList.append(labelNumber)
+            print(labelNumber)
+        else:
+            break
+        
+    #tennisball
+    if 1 in labelList:
+        print("tennisball found; do tennisball things")
+
+    #finger
+    if 2 in labelList:
+        print("finger found; do finger things")
+
+    #laugh    
+    if 3 in labelList:
+        print("laugh found; do laugh things")
+    
+####################### END OF good place for your modifications #############################
+
     # Visualization of the results of a detection.
     vis_util.visualize_boxes_and_labels_on_image_array(
         image,
@@ -77,7 +116,6 @@ while True:
         use_normalized_coordinates=True,
         line_thickness=8)
     cv2.imshow('cam picture', image) 
-    #time.sleep(0.5)
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
